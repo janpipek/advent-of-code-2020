e0363b6e7cd4b3fee119328e4a6ee5b713388c92
Jan Pipek <jan.pipek@gmail.com>
2020-12-03 20:07:22 +0100

    Solve day 1 / task a

--- advent_of_code/day1.cpp
+++ advent_of_code/day1.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <filesystem>
+#include <cstdlib>
+
+#include "lib.hh"
 
 using namespace std;
+using namespace std::filesystem;
 
 int main(int argc, char** argv) {
-	ifstream infile("data\\input_01.txt");
+	ifstream infile = openInputFile("input-01.txt");
 	string buffer;
 
+	vector<int> numbers;
+
 	while (getline(infile, buffer)) {
-		cout << buffer << endl;
-		cout << "." << endl;
+		int current = stoi(buffer);
+		numbers.push_back(current);
+
+		for (auto i : numbers) {
+			if (i + current == 2020) {
+				cout << i * current << endl;
+			}
+		}
 	}
 }
---
+++ advent_of_code/lib.hh
@@ -0,0 +1,24 @@
+#ifndef ADVENT_OF_CODE_LIB_HH
+#define ADVENT_OF_CODE_LIB_HH
+
+#include <fstream>
+#include <filesystem>
+#include <cstdlib>
+
+std::ifstream openInputFile(const std::string& basename) {
+	char* directory = getenv("ADVENT_OF_CODE_DATA_DIR");
+	if (!directory) {
+		directory = ".";
+	}
+
+	auto dataPath = std::filesystem::path(directory) / basename;
+	std::ifstream infile(dataPath);
+	std::string buffer;
+
+	if (!infile.good()) {
+		std::cerr << "File " << absolute(dataPath) << " could not be opened." << std::endl;
+		exit(-1);
+	}
+	return infile;
+}
+#endif
