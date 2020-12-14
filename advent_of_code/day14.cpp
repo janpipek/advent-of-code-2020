aa9e8fa69ff4dce9613064772e45b2401435535b
Jan Pipek <jan.pipek@gmail.com>
2020-12-14 12:49:36 +0100

    Solve day 14 - task 2

--- advent_of_code/day14.cpp
+++ advent_of_code/day14.cpp
@@ -8,9 +8,7 @@
 
 using namespace std;
 
-// const size_t MASK_LENGTH = 36;
-
-using Memory = array<unsigned long long, 65536>;
+using Memory = map<unsigned long long, unsigned long long>;
 using Mask = string;
 
 struct ProgramState {
@@ -18,8 +16,19 @@ struct ProgramState {
     Mask mask;
 };
 
-long long applyMask(Mask mask, long long number) {
-    long long result = 0;
+unsigned long long sumMemory(const Memory& memory) {
+    return accumulate(
+        memory.cbegin(),
+        memory.cend(),
+        0ULL,
+        [](auto left, auto& right) {
+            return left + right.second;
+        }
+    );
+}
+
+unsigned long long applyMask(Mask mask, long long number) {
+    unsigned long long result = 0;
     for (size_t i = 0; i < mask.size(); i++) {
         auto maskBit = mask[mask.size() - i - 1];
         if (maskBit == 'X') {
@@ -47,16 +56,47 @@ auto taskA() {
             state.mask = line.substr(7);
         }
     }
-    return accumulate(
-        state.memory.cbegin(),
-        state.memory.cend(),
-        0ULL,
-        plus<unsigned long long>()
-    );
+    return sumMemory(state.memory);
+}
+
+vector<unsigned long long> decodeAddresses(unsigned long long address, const Mask& mask) {
+    vector<unsigned long long> result { address };
+    for (size_t i = 0; i < mask.size(); i++) {
+        auto maskBit = mask[mask.size() - i - 1];
+        if (maskBit == 'X') {
+            size_t m = result.size();
+            for (size_t j = 0; j < m; j++) {
+                auto& current = result[j];
+                result.push_back(current ^ (1ULL << i));
+            }            
+        }
+        else if (maskBit == '1') {
+            size_t m = result.size();
+            for (size_t j = 0; j < m; j++) {
+                result[j] |= (1ULL << i);
+            }
+        }
+    }
+    return result;    
 }
 
 auto taskB() {
-	NOT_YET_IMPLEMENTED
+    auto lines = readLines("input-14.txt");
+    ProgramState state;
+    for (auto line : lines) {
+        if (line.substr(0, 3) == "mem") {
+            auto address_value = split(line.substr(4), "] = ");
+            unsigned long long address = stoull(address_value[0]);
+            unsigned long long value = stoll(address_value[1]);
+            for (unsigned long long address : decodeAddresses(address, state.mask)) {
+                state.memory[address] = value;
+            }
+        }
+        else if (line.substr(0, 4) == "mask") {
+            state.mask = line.substr(7);
+        }
+    }
+    return sumMemory(state.memory);
 }
 
 MAIN;
