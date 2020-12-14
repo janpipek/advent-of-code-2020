464c0c2c6b7bec01ad6088c787f699aeefde058a
Jan Pipek <jan.pipek@gmail.com>
2020-12-14 10:53:41 +0100

    Prepare for day 13

---
+++ advent_of_code/day13.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+#include "lib.hh"
+
+using namespace std;
+
+auto taskA() {
+    NOT_YET_IMPLEMENTED
+}
+
+auto taskB() {
+    NOT_YET_IMPLEMENTED
+}
+
+MAIN;
