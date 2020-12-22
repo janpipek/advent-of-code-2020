439ae4fc28dbbb70a6bba90515cb6caed2cdd102
Jan Pipek <jan.pipek@gmail.com>
2020-12-22 09:25:57 +0100

    Day21 - read input

--- advent_of_code/day21.cpp
+++ advent_of_code/day21.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
 
 #include "aoc/task.hh"
 
 using namespace std;
 
+struct Food {
+    vector<string> ingredients;
+    vector<string> allergens;
+};
+
+using Input = vector<Food>;
+
+Input readInput() {
+    Input result;
+    auto lines = readLines("input-21-test.txt");
+    for (const auto &line : lines) {
+        auto ingredients_allergens = split(line, " (contains ");
+        result.push_back({
+                                 split(ingredients_allergens[0]),
+                                 split(ingredients_allergens[1].substr(0, ingredients_allergens[1].size() - 1), ", ")
+                         });
+    }
+    return result;
+}
+
 auto taskA() {
+    auto input = readInput();
     NOT_YET_IMPLEMENTED
 }
 
