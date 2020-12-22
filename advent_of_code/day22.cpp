a1185781b879e9d8440de59e6eb7610c2460a609
Jan Pipek <jan.pipek@gmail.com>
2020-12-22 19:54:53 +0100

    Attempt to solve day 22

--- advent_of_code/day22.cpp
+++ advent_of_code/day22.cpp
@@ -11,13 +11,16 @@ using Card = uint16_t;
 using Deck = deque<Card>;
 using Input = pair<Deck, Deck>;
 
+const uint8_t PLAYER1 = 1;
+const uint8_t PLAYER2 = 2;
+
 using GameLog = vector<pair<Deck, Deck>>;
 
 void playTurn(Deck& deck1, Deck& deck2) {
     auto card1 = deck1[0];
     auto card2 = deck2[0];
     
-    cout << deck1.size() << " : " << deck2.size() << " --- " << card1 << " - " << card2 << endl;
+    debug << deck1.size() << " : " << deck2.size() << " --- " << card1 << " - " << card2 << endl;
 
     deck1.pop_front();
     deck2.pop_front();
@@ -31,13 +34,106 @@ void playTurn(Deck& deck1, Deck& deck2) {
     }
 }
 
-void playTurnWithRecursion(Deck& deck1, Deck& deck2, GameLog& log) {
-    // TODO: Implement
+u_int8_t playGame(Deck& deck1, Deck& deck2) {
+    while (true) {
+        playTurn(deck1, deck2);
+        if (deck1.empty()) {
+            return PLAYER2;
+        }
+        if (deck2.empty()) {
+            return PLAYER1;
+        }
+    }    
 }
 
-u_int8_t playRecursiveGame(Deck& deck1, Deck& deck2) {
-    // TODO: Implement
-    return 0;
+u_int8_t playRecursiveGame(Deck& deck1, Deck& deck2, size_t game = 1) {
+    static size_t maxGame = 1;
+    if (game > maxGame) {
+        maxGame = game;
+    }
+
+    debug << endl << "=== Game " << game << " ===" << endl << endl;
+
+    GameLog log {};
+
+    int round = 0;
+
+    while (true) {
+        debug << endl;
+
+        round++;
+        debug << "-- Round " << round << " (Game " << game << ") --" << endl;
+        debug << "Player 1's deck: " << deck1 << endl;
+        debug << "Player 2's deck: " << deck2 << endl;
+
+        Card& card1 = deck1[0];
+        Card& card2 = deck2[0];
+
+        debug << "Player 1 plays: " << card1 << endl;
+        debug << "Player 2 plays: " << card2 << endl;
+
+        // Check for the same situation before
+        pair<deque<Card>, deque<Card>> currentDecks{deck1, deck2};
+        auto it = find(log.cbegin(), log.cend(), currentDecks);
+        if (it != log.cend()) {
+            debug << "Same as round " << (it - log.cbegin() + 1) << endl;
+            return PLAYER1;
+        }
+
+        /*
+        // With swapped cards;
+        pair<deque<Card>, deque<Card>> revDecks{deck2, deck1};
+        it = find(log.cbegin(), log.cend(), revDecks);
+        if (it != log.cend()) {
+            debug << "Same as round " << (it - log.cbegin() + 1) << endl;
+            return PLAYER1;
+        } */       
+
+        log.push_back(currentDecks);
+
+        deck1.pop_front();
+        deck2.pop_front();
+
+        uint8_t turnWinner;
+
+        if ((deck1.size() >= card1) && (deck2.size() >= card2)) {
+            // Sub-game
+            debug << "Playing a sub-game to determine the winner..." << endl;
+
+            Deck subdeck1;
+            Deck subdeck2;
+            copy(deck1.begin(), deck1.begin() + card1, back_inserter(subdeck1));
+            copy(deck2.begin(), deck2.begin() + card2, back_inserter(subdeck2));
+
+            turnWinner = playRecursiveGame(subdeck1, subdeck2, maxGame+1);
+
+            debug << "...anyway, back to game " << game << "." << endl;
+        }
+        else {
+            // Standard
+            turnWinner = (card1 > card2) ? PLAYER1 : PLAYER2; 
+        }
+
+        if (turnWinner == PLAYER1) {
+            debug << "Player 1 wins round " << round << " of game " << game << "!" << endl;
+            deck1.push_back(card1);
+            deck1.push_back(card2);
+        } else {
+            debug << "Player 2 wins round " << round << " of game " << game << "!" << endl;
+            deck2.push_back(card2);
+            deck2.push_back(card1);
+        }
+
+        // Final winner check
+        if (deck1.empty()) {
+            debug << "The winner of game " << game << " is player 2!" << endl;
+            return PLAYER2;
+        }
+        if (deck2.empty()) {
+            debug << "The winner of game " << game << " is player 1!" << endl;
+            return PLAYER1;
+        }       
+    }
 }
 
 u_int64_t getScore(const Deck& deck) {
@@ -63,19 +159,28 @@ Input readInput() {
 auto taskA() {
     auto input = readInput();
 
-    while (true) {
-        playTurn(input.first, input.second);
-        if (input.first.empty()) {
-            return getScore(input.second);
-        }
-        if (input.second.empty()) {
-            return getScore(input.first);
-        }
+    auto winner = playGame(input.first, input.second);
+    if (winner == PLAYER1) {
+        return getScore(input.first);
+    } else {
+        return getScore(input.second);
     }
 }
 
 auto taskB() {
-    NOT_YET_IMPLEMENTED
+    auto input = readInput();
+
+    auto winner = playRecursiveGame(input.first, input.second);
+
+    debug << "== Post-game results ==" << endl;
+    debug << "Player 1's deck: " << input.first << endl; 
+    debug << "Player 2's deck: " << input.second << endl;
+
+    if (winner == PLAYER1) {
+        return getScore(input.first);
+    } else {
+        return getScore(input.second);
+    }
 }
 
 MAIN;
