7965bc388dbb248ee37fc5b8fa86e0f9f3813f96
Jan Pipek <jan.pipek@gmail.com>
2020-12-22 09:33:15 +0100

    Day22 - read input

--- advent_of_code/day22.cpp
+++ advent_of_code/day22.cpp
@@ -6,7 +6,24 @@
 
 using namespace std;
 
+using Card = uint16_t;
+using Deck = vector<Card>;
+using Input = pair<Deck, Deck>;
+
+Input readInput() {
+    auto sections = readSections("input-22.txt");
+    Input result;
+    for (auto line = sections[0].cbegin() + 1; line < sections[0].cend(); line++) {
+        result.first.push_back(stoi(*line));
+    }
+    for (auto line = sections[1].cbegin() + 1; line < sections[1].cend(); line++) {
+        result.second.push_back(stoi(*line));
+    }
+    return result;
+}
+
 auto taskA() {
+    auto input = readInput();
     NOT_YET_IMPLEMENTED
 }
 
