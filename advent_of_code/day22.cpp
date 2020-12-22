75c67aa11d5036880efbc3adfb43661dcb271003
Jan Pipek <jan.pipek@gmail.com>
2020-12-22 13:56:12 +0100

    Solve 22 - task 1

--- advent_of_code/day22.cpp
+++ advent_of_code/day22.cpp
@@ -1,3 +1,4 @@
+#include <deque>
 #include <iostream>
 #include <string>
 #include <algorithm>
@@ -7,8 +8,45 @@
 using namespace std;
 
 using Card = uint16_t;
-using Deck = vector<Card>;
+using Deck = deque<Card>;
 using Input = pair<Deck, Deck>;
+
+using GameLog = vector<pair<Deck, Deck>>;
+
+void playTurn(Deck& deck1, Deck& deck2) {
+    auto card1 = deck1[0];
+    auto card2 = deck2[0];
+    
+    cout << deck1.size() << " : " << deck2.size() << " --- " << card1 << " - " << card2 << endl;
+
+    deck1.pop_front();
+    deck2.pop_front();
+
+    if (card1 > card2) {
+        deck1.push_back(card1);
+        deck1.push_back(card2);
+    } else {
+        deck2.push_back(card2);
+        deck2.push_back(card1);
+    }
+}
+
+void playTurnWithRecursion(Deck& deck1, Deck& deck2, GameLog& log) {
+    // TODO: Implement
+}
+
+u_int8_t playRecursiveGame(Deck& deck1, Deck& deck2) {
+    // TODO: Implement
+    return 0;
+}
+
+u_int64_t getScore(const Deck& deck) {
+    u_int64_t result = 0ULL;
+    for (size_t i = 0; i < deck.size(); i++) {
+        result += (deck.size() - i) * deck[i];
+    }
+    return result;
+}
 
 Input readInput() {
     auto sections = readSections("input-22.txt");
@@ -24,7 +62,16 @@ Input readInput() {
 
 auto taskA() {
     auto input = readInput();
-    NOT_YET_IMPLEMENTED
+
+    while (true) {
+        playTurn(input.first, input.second);
+        if (input.first.empty()) {
+            return getScore(input.second);
+        }
+        if (input.second.empty()) {
+            return getScore(input.first);
+        }
+    }
 }
 
 auto taskB() {
